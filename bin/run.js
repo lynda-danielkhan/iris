dc4c114b3c9c8803436d5c79a37b99cda5e95d31
lynda-danielkhan <lynda@khan.io>
2017-02-13 11:34:13 -0800

    Initial commit

---
+++ bin/run.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const slackClient = require('../server/slackClient');
+const service = require('../server/service');
+const http = require('http');
+const server = http.createServer(service);
+
+const witToken = 'WIT_API_TOKEN';
+const witClient = require('../server/witClient')(witToken);
+
+const slackToken = 'SLACK_API_TOKEN';
+const slackLogLevel = 'verbose';
+
+const serviceRegistry = service.get('serviceRegistry');
+const rtm = slackClient.init(slackToken, slackLogLevel, witClient, serviceRegistry);
+rtm.start();
+
+slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
+
+server.on('listening', function() {
+    console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode.`);
+});
---
+++ server/slackClient.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const RtmClient = require('@slack/client').RtmClient;
+const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
+const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
+let rtm = null;
+let nlp = null;
+let registry = null;
+
+function handleOnAuthenticated(rtmStartData) {
+    console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+}
+
+function handleOnMessage(message) {
+
+    if (message.text.toLowerCase().includes('iris')) {
+        nlp.ask(message.text, (err, res) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            try {
+                if(!res.intent || !res.intent[0] || !res.intent[0].value) {
+                    throw new Error("Could not extract intent.")
+                }
+
+                const intent = require('./intents/' + res.intent[0].value + 'Intent');
+
+                intent.process(res, registry, function(error, response) {
+                    if(error) {
+                        console.log(error.message);
+                        return;
+                    }
+                    
+                    return rtm.sendMessage(response, message.channel);
+                })
+
+            } catch(err) {
+                console.log(err);
+                console.log(res);
+                rtm.sendMessage("Sorry, I don't know what you are talking about!", message.channel);
+            }
+
+        });
+    }
+
+}
+
+function addAuthenticatedHandler(rtm, handler) {
+    rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, handler);
+}
+
+
+module.exports.init = function slackClient(token, logLevel, nlpClient, serviceRegistry) {
+    rtm = new RtmClient(token, { logLevel: logLevel });
+    nlp = nlpClient;
+    registry = serviceRegistry;
+    addAuthenticatedHandler(rtm, handleOnAuthenticated);
+    rtm.on(RTM_EVENTS.MESSAGE, handleOnMessage);
+    return rtm;
+}
+
+module.exports.addAuthenticatedHandler = addAuthenticatedHandler;
