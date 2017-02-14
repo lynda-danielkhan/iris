e2e18e9abd7380523f8b7d85a0bb4b651047afad
lynda-danielkhan <lynda@khan.io>
2017-02-14 15:41:52 -0800

    Refactored SlackClient

--- bin/run.js
+++ bin/run.js
@@ -2,7 +2,7 @@
 
 const config = require('../config');
 
-const slackClient = require('../server/slackClient');
+const SlackClient = require('../server/slackClient');
 const service = require('../server/service')(config);
 const http = require('http');
 const server = http.createServer(service);
@@ -11,15 +11,15 @@ const witToken = config.witToken;
 const WitClient = require('../server/witClient');
 const witClient = new WitClient(witToken);
 
-const slackToken = config.slackToken;
-const slackLogLevel = 'verbose';
+
+
 
 const serviceRegistry = service.get('serviceRegistry');
-const rtm = slackClient.init(slackToken, slackLogLevel, witClient, serviceRegistry);
-rtm.start();
+const slackClient = new SlackClient(config.slackToken, config.slackLogLevel, witClient, serviceRegistry);
 
-
-slackClient.addAuthenticatedHandler(rtm, () => server.listen(3000));
+slackClient.start(() => {
+    server.listen(3000);
+});
 
 server.on('listening', function() {
     console.log(`IRIS is listening on ${server.address().port} in ${service.get('env')} mode.`);
